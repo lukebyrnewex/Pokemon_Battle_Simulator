80f6618874da74985e0cfff8abbd73671f18ced5
Luke Alexander Byrne <byrnel37@tcd.ie>
2020-07-18 15:18:37 +0100

    Docstrings for battle.py

--- battle.py
+++ battle.py
@@ -4,15 +4,25 @@ import pokemon
 
 class Battle:
     def __init__(self, pokemon1, pokemon2):
+        """Initialises a Battle between two Pokémon."""
         self.pokemon1 = pokemon1
         self.pokemon2 = pokemon2
 
     def battling_pokemon(self):
+        """Pretty prints the names of the battling Pokémon."""
         print(f'{self.pokemon1.name} and {self.pokemon2.name} are battling!')
 
 
-# Select a move to use in battle (returns number of move)
 def move_select(pkmn):
+    """Selects a move to use in battle from the Pokémon's move inventory.
+
+    Args:
+        pkmn (pokemon.Pokemon): The Pokémon whose move is being selected.
+
+    Returns:
+        selection (int): The index of the Pokémon's move to use.
+
+    """
     print(len(pkmn.moves))
     move_number = 0
     for move in pkmn.moves:
@@ -22,8 +32,8 @@ def move_select(pkmn):
     return selection
 
 
-# Initialise a battle sequence by selecting Pokémon
 def create_battle():
+    """Initialise a battle sequence by setting up two Pokémon."""
     pokemon1 = pokemon.pick_pokemon()
     pokemon2 = pokemon.pick_pokemon()
     pokemon1.print_pokemon()
@@ -32,8 +42,8 @@ def create_battle():
     return Battle(pokemon1, pokemon2)
 
 
-# (Very basic) implementation of an attack
 def attack(atk_pkmn, def_pkmn):
+    """Function which allows one Pokémon to attack another."""
     # Calculate the damage based on the move
     move_no = move_select(atk_pkmn)
     selected_move = atk_pkmn.moves[move_no - 1]
