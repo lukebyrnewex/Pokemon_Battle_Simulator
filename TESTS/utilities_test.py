f40bdd432c0c024ba7174d5622ed04f12be3bd3d
Luke Alexander Byrne <byrnel37@tcd.ie>
2020-07-19 21:21:57 +0100

    Docstrings for utilities_test.py

--- TESTS/utilities_test.py
+++ TESTS/utilities_test.py
@@ -1,8 +1,8 @@
 import unittest
-import csv
 from pathlib import Path
 from utilities import str_list_to_int, csv_extractor
 
+# File Processing
 data_folder = Path(
     "D:/Luke/Documents/Programming/Python/Pokemon_Battle_Simulator/CSV/")
 file_pokemon_csv = data_folder / "pokemon_list.csv"
@@ -10,7 +10,7 @@ file_pokemon_csv = data_folder / "pokemon_list.csv"
 
 class TestUtilities(unittest.TestCase):
     def test_str_list_to_int(self):
-        # TODO: docstring
+        """Tests to see whether conversion occurs correctly."""
         self.assertEqual(str_list_to_int(
             ["1", "5", "20", "50"]), [1, 5, 20, 50])
         self.assertEqual(str_list_to_int(
@@ -21,14 +21,11 @@ class TestUtilities(unittest.TestCase):
             "1"), [1])
 
     def test_csv_extractor(self):
-        # TODO: docstring
+        """Tests to see whether correct information is extracted."""
         self.assertEqual(csv_extractor(
             file_pokemon_csv, "Name", "Misdreavus", "Generation"), 2)
         self.assertEqual(csv_extractor(
             file_pokemon_csv, "Name", "Scizor", "Attack"), 130)
-
-    #def test_find_key_index(self):
-        # TODO: docstring, tests
 
 
 if __name__ == '__main__':
