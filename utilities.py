0071e70a04689721f3beb5dc0f9b7c67f7023a3f
Luke Alexander Byrne <byrnel37@tcd.ie>
2020-07-22 15:05:51 +0100

    Small int conversion error fix

--- utilities.py
+++ utilities.py
@@ -2,5 +2,5 @@ def pandas_series_to_int_list(series):
     """Transform int series into a list of ints."""
     int_list = []
     for stats in series.iteritems():
-        int_list.append(stats[1])
+        int_list.append(int(stats[1]))
     return int_list
